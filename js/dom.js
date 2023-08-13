59152c694fb1e4c44f9b3386798d4a91607bc7fc
sohel5G <contactsohelrana@gmail.com>
2023-08-13 09:06:54 +0600

    Almost done

---
+++ js/dom.js
@@ -0,0 +1,81 @@
+
+// let allH1 = document.getElementsByTagName('h1');
+// for( let h1 of allH1 ){
+//     console.log( h1 );
+// }
+
+// let allLi = document.getElementsByTagName('li');
+// for( let li of allLi ){
+//     // console.log( li );
+//     console.log( li.innerText );
+// }
+
+
+// let getByCssId = document.getElementById('section_id').innerText = 'change this text';
+// console.log( getByCssId );
+
+// let getByCssClass = document.getElementsByClassName( 'class-name' );
+// console.log( getByCssClass );
+// for(classes of getByCssClass){
+//     console.log(classes.innerText);
+// }
+
+
+// let getByCssId = document.querySelectorAll( '#section_id' );
+// console.log( getByCssId );
+
+// let getByCssClass = document.querySelectorAll( '.class-name' );
+// // console.log( getByCssClass );
+// for( let classs of getByCssClass ){
+//     console.log( classs );
+// }
+
+
+// let getById = document.querySelector('#section_id');
+// console.log( getById );
+
+// let getByClasss = document.querySelector('.class-name h4').style.color='red';
+
+// document.getElementById('section_id').style.color='red';
+// document.getElementById( 'section_id' ).style.backgroundColor='gray';
+
+// let title = document.getElementById('one');
+// console.log( title.getAttribute('id') );
+// console.log( title.getAttribute('class') );
+
+// console.log( title.classList );
+// console.log( title.classList.remove('cl2') );
+// console.log( title.classList );
+// console.log( title.classList.add('purple-blue') );
+// console.log( title.classList );
+
+// console.log( title.setAttribute('title', ' this is test attribute') );
+
+// let classs = document.getElementsByClassName('class-name');
+// console.log( classs[1].innerHTML );
+// console.log( classs[1].innerText );
+
+// console.log( classs[0].innerHTML='<b>This text replace by JS</b>' );
+
+
+// let getAllSections = document.querySelectorAll('section');
+// console.log(getAllSections);
+
+// for( let section of getAllSections ){
+//     section.style.border='1px solid red';
+//     section.style.backgroundColor='blue';
+//     section.style.margin='20px';
+// }
+
+
+
+
+let getAllClassH1 = document.querySelectorAll('.class-name h4');
+for(let x of getAllClassH1){
+    x.style.color='blue';
+    x.style.textAlign='center';
+}
+
+
+
+
