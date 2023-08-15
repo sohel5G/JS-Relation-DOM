0883ab58e76ab6b841a412cdccf95e8d013e7b22
sohel5G <contactsohelrana>
2023-08-15 11:32:42 +0600

    Hello

--- js/new.js
+++ js/new.js
@@ -25,8 +25,40 @@
 // console.log(  document.getElementsByClassName('lies')[0].innerHTML = '<h1>Hello this text by JavaScript</h1>'   );
 
 
+// document.getElementById('main').style.backgroundColor='blue';
+// for(let aa of document.querySelectorAll( '#main ul li a' )){
+//     aa.style.color='white';
+//     aa.style.fontSize='25px';
+// }
+// for(let aa of document.querySelectorAll( '#main ul' )){
+//     aa.style.listStyle='none';
+// }
 
 
+
+// document.getElementById('main').classList.add('new-class');
+// document.getElementById('main').classList.remove('lies');
+
+
+// console.log(  document.getElementById('main')   );
+// console.log(  document.getElementById('main').innerHTML   );
+// console.log(  document.getElementById('main').innerText   );
+console.log(  document.getElementById('main').childNodes   );
+console.log(  document.getElementById('main').parentNode   );
+// console.log(  document.getElementById('main').childNodes[1]   );
+// console.log(  document.getElementById('main').childNodes[1].childNodes   );
+
+// console.log(  document.querySelector('#main').createElement('p')   );
+
+
+
+let li = document.createElement('li'); 
+li.innerText='Hello World';
+document.querySelector('#main ul').append(li)
+
+let li2 = document.createElement('li');
+li2.innerText = 'Hello World 2';
+document.querySelector('#main ul').append(li2);
 
 
 
